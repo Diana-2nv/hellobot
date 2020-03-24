775bde617e860ce628254f5b53d14d64406d1330
dianamlopezb <dianamlopez@gmail.com>
2020-03-23 23:06:46 -0500

    webargs

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from flask import Flask
-from flask_apispec import use_kwargs
+from webargs import fields
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World Adding flask-apispec!"
+    return "Hello World Adding webargs!"
