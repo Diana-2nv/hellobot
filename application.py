f34739aa35bb34077197f0bf59df5a7604926007
dianamlopezb <dianamlopez@gmail.com>
2020-03-24 15:13:42 -0500

    Origina

--- application.py
+++ application.py
@@ -1,8 +1,7 @@
 from flask import Flask
-from flask_apispec import use_kwargs
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World Adding webargs!"
+    return "Hello World 01"
